59c4be69b6991ecd7eb1c04b597807876e8ef734
Fon Godwill ivan Tana <84873112+ivan-tana@users.noreply.github.com>
2023-07-22 19:45:06 +0100

    main blueprint

--- src/app/__init__.py
+++ src/app/__init__.py
@@ -1,10 +1,14 @@
 from flask import Flask
+from app.blueprints import blueprints
 
 
 def create_app(config_file='config.py'):
     app = Flask(__name__)
 
-
     app.config.from_pyfile(config_file)
+
+    # register blueprints 
+    for blueprint in blueprints:
+        app.register_blueprint(blueprint)
 
     return app
