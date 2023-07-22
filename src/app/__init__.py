d9fb22faf893c1aa446a250bb667d7215c443293
Fon Godwill ivan Tana <84873112+ivan-tana@users.noreply.github.com>
2023-07-22 21:18:42 +0100

    init databse extension

--- src/app/__init__.py
+++ src/app/__init__.py
@@ -1,6 +1,6 @@
 from flask import Flask
 from app.blueprints import blueprints
-
+from app import extensions
 
 def create_app(config_file='config.py'):
     app = Flask(__name__)
@@ -10,5 +10,7 @@ def create_app(config_file='config.py'):
     # register blueprints 
     for blueprint in blueprints:
         app.register_blueprint(blueprint)
+
+    extensions.db.init_app(app)
 
     return app
---
+++ src/app/config.py
@@ -0,0 +1,3 @@
+SQLALCHEMY_DATABASE_URI = "sqlite3:///database.db"
+SQLALCHEMY_TRACK_MODIFICATION = False
+SECRET_KEY = "dfby678nd4569mhtybc45vtd"
